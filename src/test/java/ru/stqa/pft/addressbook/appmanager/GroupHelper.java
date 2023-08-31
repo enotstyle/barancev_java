8ee511f71b4c644607214a49f3b6820a9431c839
Ivan <vamek-ter@mail.ru>
2023-08-31 20:49:13 +0700

    Добавлена проверка на соответвие списков групп после удаления одной группы

--- src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // помогает работать с группами
 public class GroupHelper extends HelperBase {
@@ -54,5 +58,16 @@ public class GroupHelper extends HelperBase {
 
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element: elements) {
+            String name = element.getText();
+            GroupData group = new GroupData(name, null, null);
+            groups.add(group);
+        }
+        return groups;
     }
 }
